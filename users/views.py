5e47a0308da9f9b74f11d51b424b73d429c4f388
m1nam1 <info@minami.xyz>
2017-03-06 08:39:33 +0900

    #21 ユーザーリストページにページングを導入

--- users/views.py
+++ users/views.py
@@ -16,6 +16,20 @@ def list(request):
     ''' ユーザーリスト '''
     # 有効なアカウントを取得
     users = CustomUser.objects.filter(is_active=True).exclude(pk=1)
+
+    # ページング
+    paginator = Paginator(users, 50)
+    page = request.GET.get('page')
+
+    try:
+        users = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        users = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        users = paginator.page(paginator.num_pages)
+
     context = { 'users': users }
     return render(request, 'users/list.html', context)
 
