90e1f8cb24494943c7a92066de93a910d4847a46
m1nam1 <info@minami.xyz>
2017-04-12 01:07:31 +0900

    adminページのURLを変更

--- srandom/urls.py
+++ srandom/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url('', include('main.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^mastermind/', admin.site.urls),
     url(r'^auth/', include('django.contrib.auth.urls', namespace='auth')),
     url(r'^social/', include('social.apps.django_app.urls', namespace='social')),
     url(r'^users/', include('users.urls')),
