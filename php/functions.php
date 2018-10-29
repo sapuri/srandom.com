d25c173bac29cb2cf0881ff5cf6413ad84cf8d47
sapuri <info@minami.xyz>
2018-10-30 03:11:13 +0900

    Remove php files

--- php/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-function connectDb(){
-	try{
-		return new PDO(DSN, DB_USER, DB_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-	}catch (PDOException $e){
-		echo $e->getMessage();
-		exit;
-	}
-}
