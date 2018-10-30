9f4c6322ca9efcb3de227c3721c5582b9b502e85
sapuri <info@minami.xyz>
2018-10-30 12:29:08 +0900

    fix static files settings

--- srandom/settings.py
+++ srandom/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'srandom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -137,13 +137,9 @@ except ImportError:
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATICFILES_FINDERS = (
-    'compressor.finders.CompressorFinder',
-)
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # メッセージ設定
