336e78dc28a3d80846a08f79939356024672202f
sapuri <info@minami.xyz>
2019-05-03 21:47:39 +0900

    feat(main/commands): support wiki relocation, and improve scraping algorithm

--- main/management/commands/scraping_old.py
+++ main/management/commands/scraping_old.py
@@ -20,8 +20,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         silent = options['silent']
 
-        url1 = 'https://popn.hyrorre.com/%E3%81%9D%E3%81%AE%E4%BB%96/s%E4%B9%B1%E3%82%AF%E3%83%AA%E3%82%A2%E9%9B%A3%E6%98%93%E5%BA%A6%E8%A1%A8'  # S乱クリア難易度表
-        url2 = 'https://popn.hyrorre.com/%E3%81%9D%E3%81%AE%E4%BB%96/s%E4%B9%B1lv0%E9%9B%A3%E6%98%93%E5%BA%A6%E8%A1%A8'  # S乱Lv0難易度表
+        url1 = 'https://hellwork.jp/popn/wiki/734.html'  # S乱クリア難易度表
+        url2 = 'https://hellwork.jp/popn/wiki/?%E3%81%9D%E3%81%AE%E4%BB%96/S%E4%B9%B1Lv0%E9%9B%A3%E6%98%93%E5%BA%A6' \
+               '%E8%A1%A8'  # S乱Lv0難易度表
         file_path = f'{settings.BASE_DIR}/csv/srandom.csv'
 
         csv_data = self.scrape(url1, mode=1, silent=silent)
@@ -46,49 +47,55 @@ class Command(BaseCommand):
 
         level_range = []
         if mode == 1:
-            # Lv19〜Lv5を取得
-            # #lv19 〜 #lv5 を指定
-            level_range = range(19, 4, -1)
+            # Lv5〜Lv19を取得
+            # #content_1_2 〜 #content_1_16 を指定
+            level_range = range(2, 17)
         elif mode == 2:
             # Lv4〜Lv1を取得
-            # #lv4 〜 #lv1 を指定
-            level_range = ['4', '3', '2強', '2弱', '1強', '1弱']
+            # #content_1_1 〜 #content_1_6 を指定
+            level_range = range(1, 7)
         else:
             print(f'invalid mode: {mode}')
             quit(1)
 
         for i, j in enumerate(level_range):
             # Lv表記を取得
-            h2 = soup.select(f'h2#lv{j}')[0]
-            level = h2.text[1:].strip()
+            h3 = soup.select(f'h3#content_1_{j}')[0].text[1:-1].strip()
             if not silent:
-                print(level)
+                print(h3)
 
             if mode == 2:
-                if j == '2強' or j == '2弱':
-                    level = 'Lv2'
-                elif j == '1強' or j == '1弱':
-                    level = 'Lv1'
+                if j == 3:  # Lv2強
+                    h3 = 'Lv2'
+                elif j == 5:  # Lv1強
+                    h3 = 'Lv1'
 
-            if mode == 2 and (j == '2弱' or j == '1弱'):
+            if mode == 2 and (j == 4 or j == 6):
                 music_list = []
             else:
-                music_list = [[level]]
+                music_list = [[h3]]
 
-            # table の tr を走査
-            table = h2.next_sibling.next_sibling.table.children
-            for i, tr in enumerate(table):
-                if i < 4 or tr == '\n':
-                    continue
+            # div.table-responsive の tr を走査
+            tr_addr = 1  # tr の場所を指定
+            while 1:
+                try:
+                    tr = soup.find_all('div', class_='table-responsive')[i].tbody.find_all('tr')[tr_addr]
+                except IndexError:
+                    if not music_list:
+                        print('music list not found')
+                        quit(1)
+                    break
 
                 td_list = tr.find_all('td')
                 lv = td_list[0].text.strip()
-                title = td_list[1].a.text.strip()
+                title = td_list[1].span.text.strip()
                 bpm = td_list[2].text.strip()
                 music_list.append([lv, title, bpm])
 
                 if not silent:
                     print(title)
+
+                tr_addr += 1
 
             csv_data.append([music_list])
 
