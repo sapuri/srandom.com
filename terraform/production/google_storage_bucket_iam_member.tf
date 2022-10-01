7500c80b044a03378aedd0b8a5f84109e817d1e7
sapuri <tappuri.sapuri@gmail.com>
2022-10-01 20:02:17 +0900

    terraform: fix IAM role for internal bucket

--- terraform/production/google_storage_bucket_iam_member.tf
+++ terraform/production/google_storage_bucket_iam_member.tf
@@ -3,3 +3,9 @@ resource "google_storage_bucket_iam_member" "all_users_are_legacy_object_reader_
   role   = "roles/storage.legacyObjectReader"
   member = "allUsers"
 }
+
+resource "google_storage_bucket_iam_member" "cloud_run_app_is_storage_admin_of_internal" {
+  bucket = google_storage_bucket.internal.name
+  role   = "roles/storage.admin"
+  member = "serviceAccount:${google_service_account.cloud_run_app.email}"
+}
