ac8ed39f36c623b22d4d9a0f95d11be6ebc27f47
sapuri <tappuri.sapuri@gmail.com>
2026-08-17 01:41:56 +0900

    remove unused cmd artifact registry repository

--- terraform/production/google_artifact_registry_repository.tf
+++ terraform/production/google_artifact_registry_repository.tf
@@ -17,23 +17,3 @@ resource "google_artifact_registry_repository" "app" {
     }
   }
 }
-
-resource "google_artifact_registry_repository" "cmd" {
-  location      = local.region
-  repository_id = "cmd"
-  format        = "DOCKER"
-  cleanup_policies {
-    id     = "delete"
-    action = "DELETE"
-    condition {
-      older_than = "86400s" # 1 day
-    }
-  }
-  cleanup_policies {
-    id     = "keep-minimum-versions"
-    action = "KEEP"
-    most_recent_versions {
-      keep_count = 1
-    }
-  }
-}
